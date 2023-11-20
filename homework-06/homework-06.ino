8862707766d37960d0c436dca701a6abaa7e3158
VladWero08 <vlad_wero@yahoo.com>
2023-11-20 17:35:43 +0200

    finished EEPROM functionalities, added EEPROM data deletion for logs, added more comments

--- homework-06/homework-06.ino
+++ homework-06/homework-06.ino
@@ -17,52 +17,47 @@ const int LED_channel_active = 255;   // channel color intensity when active
 const int LED_channel_inactive = 0;   // channel color intensity when inactive
 bool LED_toggle_mode_state = LOW;
 
+// default value for sensor settings (threshold, sampling)
+const int sensor_value_default = -1;
 // PIN value for the photocell sensor
-const int photocell_pin = 0;            // connected to A0
+const int photocell_pin = 0;                    // connected to A0
 int photocell_value;
-int photocell_threshold;                // maximum value allowed for the photocell
-bool is_photocell_threshold_respected;  // HIGH if the environment respects sensor's threshold
+int photocell_threshold = sensor_value_default; // maximum value allowed for the photocell
+bool is_photocell_threshold_respected;          // HIGH if the environment respects sensor's threshold
 
 // PIN values for HC-SR04 ultrasonic sensor
-const int trig_pin = 5;                 // trig pin 
-const int echo_pin = 6;                 // echo pin 
-long ultrasonic_duration;               // duration of the signal travel
-int ultrasonic_distance;                // actual distance registered by the ultrasonic
-int ultrasonic_threshold;               // maximum distance allowed for the ultrasonic 
+const int trig_pin = 5;                          // trig pin 
+const int echo_pin = 6;                          // echo pin 
+long ultrasonic_duration;                        // duration of the signal travel
+int ultrasonic_distance;                         // actual distance registered by the ultrasonic
+int ultrasonic_threshold = sensor_value_default; // maximum distance allowed for the ultrasonic 
 const float ultrasonic_speed_factor = 0.034 / 2;
 bool is_ultrasonic_threshold_respected; // HIGH if the environment respects sensor's threshold
 // samping rate per seconds
-int sampling_rate;
+int sampling_rate = sensor_value_default;
 
+// number of sesnors for the system
+const int number_of_sensors = 2;
 // maximum number of logs for each sensor
 int maximum_logs = 10;
 unsigned long int last_long_displayed = 0;
 // where data is stored from EEPROM reads
 int photocell_counter_value, ultrasonic_counter_value;
-/* because the logs of the ultrasonic will contain
+/* because the logs of the photocell will contain
 integers, which are stored on 2 bytes, 
-from byte 0-19, ultrasonic logs */
-int eeprom_ultrasonic_address_counter = 0;
-int eeprom_ultrasonic_address;          // actual address of the ultrasonic logs
-// from byte 20-39, photocell logs
+from byte 0-19, photocell logs */
+const int eeprom_photocell_memory_space = sizeof(int) * maximum_logs;
 int eeprom_photocell_address_counter = 0;
 int eeprom_photocell_address;           // actual address of the photocell logs
+// from byte 20-39, ultrasonic logs
+int eeprom_ultrasonic_address_counter = 0;
+int eeprom_ultrasonic_address;          // actual address of the ultrasonic logs
 
-/* variables in which the user input
-will be read into */
+// variable for user input reads
 int user_input_int;
-float user_input_float;
-String user_input_str;
+
 // current menu that is being displayed
 unsigned int current_menu = 0;
-
-/* variables related to errors of each menu of 
-the program: main, sensor, logger, system and RGB LED menus*/
-String main_menu_error_input = "Invalid input! Number in interval [1-4] is expected.";
-String sensor_menu_error_input = "Invalid input! Number in interval [1-4] is expected.";
-String logger_menu_error_input = "Invalid input! 0 or 1 are expected.";
-String system_menu_error_input = "Invalid input! Number in interval [1-4] is expected.";
-String RGB_LED_menu_error_input = "Invalid input! Number in interval [1-3] is expected.";
 
 void setup() {
   pinMode(LED_red_pin, OUTPUT);
@@ -118,7 +113,7 @@ void handle_user_input(){
       handle_user_input_on_system_menu();
       break;
     case 31:
-      handle_user_input_sensor_readings();
+      handle_user_on_input_sensor_readings();
       break;
     case 4:
       handle_user_input_on_RGB_LED_menu();
@@ -137,7 +132,7 @@ void handle_user_input_on_main_menu(){
   if (user_input_int < 1 || user_input_int > 4) {
     /* user input is not valid, print an error
     message and exit the function */
-    Serial.println(main_menu_error_input);
+    Serial.println("Invalid input! Number in interval [1-4] is expected.");
     return;
   }
 
@@ -167,7 +162,7 @@ void handle_user_input_on_sensor_menu(){
   if (user_input_int < 1 || user_input_int > 4) {
     /* user input is not valid, print an error
     message and exit the function */
-    Serial.println(sensor_menu_error_input);
+    Serial.println("Invalid input! Number in interval [1-4] is expected.");
     return;
   }
 
@@ -241,12 +236,17 @@ void handle_user_input_on_logger_menu(){
   if (user_input_int != 0 && user_input_int != 1) {
     /* user input is not valid, print an error
     message and exit the function */
-    Serial.println(logger_menu_error_input);
+    Serial.println("Invalid input! 0 or 1 are expected.");
     return;
   }
 
   if (user_input_int == 0) {
-    Serial.println("yes");
+    /* reset the EEPROM memory; because the value of the sensors
+    will be stored as integers, the jumps in memory will be of the size
+    of an integer (2 bytes)*/
+    for (int i = 0; i < sizeof(int) * number_of_sensors * maximum_logs; i = i + sizeof(int)){
+      EEPROM.put(i, 0);
+    }
   } 
 
   current_menu = 1;
@@ -258,7 +258,7 @@ void handle_user_input_on_system_menu(){
   if (user_input_int < 1 || user_input_int > 4) {
     /* user input is not valid, print an error
     message and exit the function */
-    Serial.println(system_menu_error_input);
+    Serial.println("Invalid input! Number in interval [1-4] is expected.");
     return;
   } 
 
@@ -279,6 +279,20 @@ void handle_user_input_on_system_menu(){
       break;
     default:
       break;
+  }
+}
+
+/* 
+  Reads the user input, and if the exit code
+  was entered, switch the menu back to the `System Status` menu.
+*/
+void handle_user_on_input_sensor_readings(){
+  user_input_int = Serial.parseInt();
+
+  if (user_input_int == 1) {
+    // exit if the user chose to exit 
+    current_menu = 3;
+    return;
   }
 }
 
@@ -306,43 +320,43 @@ void display_current_sensor_readings(){
   }
 }
 
-/* 
-  Reads the user input, and if the exit code
-  was entered, switch the menu back to the `System Status` menu.
-*/
-void handle_user_input_sensor_readings(){
-  user_input_int = Serial.parseInt();
-
-  if (user_input_int == 1) {
-    // exit if the user chose to exit 
-    current_menu = 3;
-    return;
-  }
-}
-
 void display_current_sensor_settings(){
   Serial.print("Sampling rate: ");
-  Serial.print(sampling_rate);
-  Serial.println("/sec");
+  if (sampling_rate != sensor_value_default) {
+    Serial.print("1 log/")
+    Serial.print(sampling_rate);
+    Serial.println(" sec");
+  } else {
+    Serial.println("unset");
+  }
 
   Serial.print("Ultrasonic threshold: ");
-  Serial.println(ultrasonic_threshold);
+  if (ultrasonic_threshold != sensor_value_default) {
+    Serial.println(ultrasonic_threshold);
+  } else {
+    Serial.println("unset");
+  }
   
   Serial.print("LDR sensor threshold: ");
-  Serial.println(photocell_threshold);
+  if (photocell_threshold != sensor_value_default) {
+    Serial.println(photocell_threshold);
+  } else {
+    Serial.println("unset");
+  }
+
   Serial.println();
 }
 
 void display_logged_data(){
   for(int i = 0; i < maximum_logs; i++){
     // calculate the index address for each logs for the photocell
-    int photocell_index_address = i * 2;
+    int photocell_index_address = i * sizeof(int);
     /* add the maximum number of logs for a photocell to
     get the address for the ultrasonic sensor */
-    int ultrasonic_index_address = photocell_index_address + 2 * maximum_logs;
+    int ultrasonic_index_address = photocell_index_address + sizeof(int) * maximum_logs;
 
-    photocell_counter_value = EEPROM.read(photocell_index_address);
-    ultrasonic_counter_value = EEPROM.read(ultrasonic_index_address);
+    EEPROM.get(photocell_index_address, photocell_counter_value);
+    EEPROM.get(ultrasonic_index_address, ultrasonic_counter_value);
 
     Serial.print(i + 1);
     Serial.println("th iteration:");
@@ -360,7 +374,7 @@ void handle_user_input_on_RGB_LED_menu(){
   if (user_input_int < 1 || user_input_int > 3) {
     /* user input is not valid, print an error
     message and exit the function */
-    Serial.println(RGB_LED_menu_error_input);
+    Serial.println("Invalid input! Number in interval [1-3] is expected.");
     return;
   }
 
@@ -453,17 +467,28 @@ void write_to_RGB_LED(){
     /* check if the threshold for the 
     sensors is respected*/
     if (is_photocell_threshold_respected == false || is_ultrasonic_threshold_respected == false){
+      /* make the button red if at least one sensor
+      is not respecting the threshold */
       analogWrite(LED_red_pin, LED_channel_active);
       analogWrite(LED_green_pin, LED_channel_inactive);
     } else {
+      // otherwise, make it green
       analogWrite(LED_red_pin, LED_channel_inactive);
       analogWrite(LED_green_pin, LED_channel_active);
     }
 
+    // blue channel will alwasy be inactive in this configuration
     analogWrite(LED_blue_pin, LED_channel_inactive);
   }
 }
 
+/*
+  Reads the value of the photocell sensor
+  and store it as a log in the EEPROM. 
+
+  Previously written logs will be overridden if
+  the maximum number of logs is passed.
+*/
 void read_photocell_sensor(){
   photocell_value = analogRead(photocell_pin);
 
@@ -477,11 +502,18 @@ void read_photocell_sensor(){
   because if the number of logs will exceed the maximum logs,
   previous logs will be overwritten */ 
   eeprom_photocell_address = eeprom_photocell_address_counter % (2 * maximum_logs);
-  // EEPROM.update(eeprom_photocell_address, photocell_value);
+  EEPROM.put(eeprom_photocell_address, photocell_value);
   // go to the next address space for an integer
   eeprom_photocell_address_counter += 2;
 }
 
+/*
+  Reads the value of the ultrasonic sensor 
+  and store it as a log in the EEPROM. 
+  
+  Previously written logs will be overridden if
+  the maximum number of logs is passed.
+*/
 void read_ultrasonic_sensor(){
   // clear the trig pin
   digitalWrite(trig_pin, LOW);
@@ -508,9 +540,9 @@ void read_ultrasonic_sensor(){
   /* write the value as a log at % 2 * maximum_logs,
   because if the number of logs will exceed the maximum logs,
   previous logs will be overwritten; also, + (2 * maximum_logs) is 
-  necessary to go after the addresses allocated for the photocell*/ 
-  eeprom_ultrasonic_address = eeprom_ultrasonic_address_counter % (2 * maximum_logs) + (2 * maximum_logs); 
-  // EEPROM.update(eeprom_ultrasonic_address, ultrasonic_distance);
+  necessary to go after the addresses allocated for the photocell */ 
+  eeprom_ultrasonic_address = eeprom_ultrasonic_address_counter % (sizeof(int) * maximum_logs) + eeprom_photocell_memory_space; 
+  EEPROM.put(eeprom_ultrasonic_address, ultrasonic_distance);
 
   // go to the next address space for an integer
   eeprom_ultrasonic_address_counter += 2;
@@ -523,7 +555,7 @@ void read_ultrasonic_sensor(){
 void choose_menu_to_display(){
   switch (current_menu) {
     case 0:
-      Serial.println("Press the labeled number to enter in the desired menu:");
+      Serial.println("MAIN:");
       Serial.println("1. Sensor Settings");
       Serial.println("2. Reset Logger Data");
       Serial.println("3. System Status");
@@ -531,54 +563,54 @@ void choose_menu_to_display(){
       Serial.println();
       break;
     case 1:
-      Serial.println(">>> Sensor Settings:");
-      Serial.println(">>> 1. Sensor Sampling Interval");
-      Serial.println(">>> 2. Ultrasonic Alert Threshold");
-      Serial.println(">>> 3. LDR Alert Threshold");
-      Serial.println(">>> 4. Back");
+      Serial.println("> Sensor Settings:");
+      Serial.println("> 1. Sensor Sampling Interval");
+      Serial.println("> 2. Ultrasonic Alert Threshold");
+      Serial.println("> 3. LDR Alert Threshold");
+      Serial.println("> 4. Back");
       Serial.println();
       break;
     case 11:
-      Serial.println(">>> Sensor Settings > Sensor Sampling Interval");
+      Serial.println("> Sensor Settings > Sensor Sampling Interval");
       Serial.println("Enter interval [1-10 integer]: ");
       Serial.println();
       break;
     case 12:
-      Serial.println(">>> Sensor Settings > Ultrasonic Sampling Threshold");
+      Serial.println("> Sensor Settings > Ultrasonic Sampling Threshold");
       Serial.println("Enter Ultrasonic sensor MAX threshold [0, 400 integer]: ");
       Serial.println();
       break;
     case 13:
-      Serial.println(">>> Sensor Settings > LDR Alert Threshold");
+      Serial.println("> Sensor Settings > LDR Alert Threshold");
       Serial.println("Enter LDR sensor MAX threshold [0, ... integer]: ");
       Serial.println();
       break;
     case 2:
-      Serial.println(">>> Reset Logger Data:");
-      Serial.println(">>> Are you sure you want to reset the logs?");
-      Serial.println(">>> 1. Yes. (0)");
-      Serial.println(">>> 2. No. (1)");
+      Serial.println("> Reset Logger Data:");
+      Serial.println("> Are you sure you want to reset the logs?");
+      Serial.println("> 1. Yes. (0)");
+      Serial.println("> 2. No. (1)");
       Serial.println();
       break;
     case 3:
-      Serial.println(">>> System Status:");
-      Serial.println(">>> 1. Current Sensor Readings");
-      Serial.println(">>> 2. Current Sensor Settings");
-      Serial.println(">>> 3. Display Logged Data");
-      Serial.println(">>> 4. Back");
+      Serial.println("> System Status:");
+      Serial.println("> 1. Current Sensor Readings");
+      Serial.println("> 2. Current Sensor Settings");
+      Serial.println("> 3. Display Logged Data");
+      Serial.println("> 4. Back");
       Serial.println();
       break;
     case 4:
-      Serial.println(">>> RGB LED Control:");
-      Serial.println(">>> 1. Manual Color Control");
-      Serial.println(">>> 2. LED: Toggle Automatic ON/OFF");
-      Serial.println(">>> 3. Back");
+      Serial.println("> RGB LED Control:");
+      Serial.println("> 1. Manual Color Control");
+      Serial.println("> 2. LED: Toggle Automatic ON/OFF");
+      Serial.println("> 3. Back");
       Serial.println();
       break;
     case 41:
-      Serial.println(">>> RGB LED Control > Manual Color Control");
+      Serial.println("> RGB LED Control > Manual Color Control");
       Serial.println("Enter values for the RGB LED between [0-255] as follows:");
-      Serial.print("`255 255 255`, 1st: R, 2nd: G, 3rd B: ");
+      Serial.println("`255 255 255`, 1st: R, 2nd: G, 3rd B: ");
       Serial.println();
       break;
     default:
